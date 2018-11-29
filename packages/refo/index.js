d979ad139611e08c4ccf291fda72841e1b7549ff
Erik Engi <kireerike@gmail.com>
2018-11-29 01:22:25 +0100

    Main command line options module loading improved

--- packages/refo/index.js
+++ packages/refo/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const fs = require('fs')
+, path = require('path')
+
 , optionsFile = './refo.js'
 
 if (fs.existsSync(optionsFile))
-	var options = eval(fs.readFileSync(optionsFile, 'UTF-8'))
+	var options = require(path.resolve(fs.realpathSync(process.cwd()), optionsFile))
 
 const main = require('refo-main')
 
