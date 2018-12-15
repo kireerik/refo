9ff54894cc335decb0601e148d94880f724f451f
Erik Engi <kireerike@gmail.com>
2018-12-15 19:26:58 +0100

    Example menu style consistency improved

--- example/asset/main.js
+++ example/asset/main.js
@@ -3,7 +3,7 @@ const {html} = require('common-tags')
 module.exports = (content, {index, resume} = {}) => html`<body>
 	<nav id="header">
 		<nav>
-			<a class="brand${index && ' selected'}" title="Home" href="/" rel="noopener">Home</a>
+			<a class="${index && 'selected'}" title="home" href="/" rel="noopener">home</a>
 			<a class="floatRight${resume && ' selected'}" title="View Richard Hendriks’s resume" href="/resume" rel="noopener">resume</a>
 		</nav>
 	</nav>
